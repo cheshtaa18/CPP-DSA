14fa1ca48ccf596cddc2c8dc315856db23e9560b
Cheshta Gupta <124520131+cheshtaa18@users.noreply.github.com>
2025-10-11 15:07:26 +0000

    add file is added into repository.

---
+++ add.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1, num2;
+    int sum = 0;
+
+    cout<<"Enter number 1: ";
+    cin >> num1;
+
+    cout << "Enter number 2: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Sum: "<< sum;
+
+    return 0;
+}
